e80747ff582e59af84fab3be202b186d20f89849
esther-v <esvalas@gmail.com>
2021-09-14 19:13:27 +0200

    mise en place sections realisations

---
+++ script.js
@@ -0,0 +1,80 @@
+class Portfolio{
+
+
+    constructor(selector){
+        this.activeContent = null
+        this.activeItem = null
+        this.container = document.querySelector(selector);
+        if (this.container === null) {
+            throw new Error(`L'élément ${selector} n'existe pas`)
+        }
+        this.children = Array.prototype.slice.call(this.container.querySelectorAll('.js-item'));
+        this.children.forEach((child) => {
+            child.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.show(child)
+            } )
+        })
+        //on clique sur un projet js-item
+         // on supp l'élément actif
+
+         // on insère ce clone après mon élément js-item
+    }
+    //Affiche le contenu d'un projet au clic
+    show(child) {
+        let offset = 0
+        if (this.activeContent !== null) {
+            this.slideUp(this.activeContent)
+            if(this.activeContent.offsetTop < child.offsetTop) {
+                offset = this.activeContent.offsetHeight
+            }
+        }
+        if (this.activeItem === child) {
+            this.activeContent = null
+            this.activeItem = null
+        } else {
+            let content = child.querySelector('.js-body').cloneNode(true)
+            child.after(content)
+            this.slideDown(content)
+            this.scrollTo(child, offset)
+            this.activeContent = content
+            this.activeItem = child
+        }
+        
+    }
+    //Affiche l'élement avec un effet d'animation
+    slideDown (element) {
+        let height = element.offsetHeight
+        element.style.height = '0px'
+        element.style.transitionDuration = '.5s';
+        element.offsetHeight // Force le repaint
+        element.style.height = height + 'px';
+        window.setTimeout( () => {
+            element.style.height = null
+        }, 500)
+    }
+
+    //Masquer un élément avec un effet d'animation
+    slideUp (element) {
+        let height = element.offsetHeight
+        element.style.height = height + 'px';
+        element.offsetHeight // Force le repaint
+        element.style.height = '0px'
+        window.setTimeout( () => {
+            element.parentNode.removeChild(element)
+        }, 500)
+    }
+
+    //Fait défiler la fenêtre jusqu'à l'élément
+    scrollTo (element, offset = 0) {
+        window.scrollTo({
+            behavior : "smooth",
+            left : 0,
+            top : element.offsetTop - offset
+        })
+    }
+
+}
+
+new Portfolio('#js-portfolio')
+
